10625b6bd688d8fb8cf2a8faa79961530649d170
Roman Susi <roman.susi@kolumbus.fi>
2018-01-21 09:51:15 +0200

    More for library

--- oMiser/mockups/python/library.py
+++ oMiser/mockups/python/library.py
@@ -5,19 +5,34 @@ cK = c(E, ARG)  # K-combinator
 cSpart = c(C, c(cK, e(ARG)))
 cS = c(C, c(e(C), c(C, c(c(E, cSpart), e(cSpart)))))  # S-combinator
 
+# boolean logic (buggy)
+cT = cK
+cF = c(C, c(e(c(e(c(E, ARG)), ARG)), c(C, c(c(E, ARG), e(ARG)))))
+cNOT = c(c(e(c(E, ARG)), ARG), c(e(c(E, ARG)), ARG))
+
 # Buggy:
-#cREV = (^cS(^cK(^cS(^cI))))(^cK)
-cREV = c(c(e(e(c(C, c(e(c(e(c(c(e(c(E, ARG)), ARG), c(e(c(E, ARG)), ARG))), ARG)), c(C, c(c(E, ARG), e(ARG))))))), ARG), c(e(c(E, ARG)), ARG))
+# cREV = (^cS(^cK(^cS(^cI))))(^cK)
+cREV = c(c(e(e(c(C, c(e(c(e(c(c(e(c(E, ARG)), ARG), c(e(c(E, ARG)), ARG))), ARG)), c(C, c(c(E, ARG), e(ARG))))))), ARG),
+         c(e(c(E, ARG)), ARG))
 
 # BCKW system
 
 # B = S (K S) K
-cB = c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG)))))))))), ARG), c(e(c(E, ARG)), ARG))
+cB = c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG)))))))))), ARG),
+       c(e(c(E, ARG)), ARG))
 
 # C = S (S (K (S (K S) K)) S) (K K)
-cC = c(c(e(c(c(e(e(c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG)))))))))), ARG), c(e(c(E, ARG)), ARG)))), ARG), c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG))), ARG), c(e(e(c(E, ARG))), ARG))
+cC = c(c(e(c(c(e(e(
+    c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG)))))))))), ARG),
+      c(e(c(E, ARG)), ARG)))), ARG),
+             c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG))), ARG),
+       c(e(e(c(E, ARG))), ARG))
 
 # W = S S (S K)
-cW = c(c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG), c(e(c(C, c(e(c(e(c(E, ARG)), ARG)), c(C, c(c(E, ARG), e(ARG)))))), ARG))
+cW = c(c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG),
+       c(e(c(C, c(e(c(e(c(E, ARG)), ARG)), c(C, c(c(E, ARG), e(ARG)))))), ARG))
+
+# TODO: iota
+# cIOTA = ...
 
 namespace = {k: v for k, v in vars().items() if isinstance(v, ob)}
