0e4c09367b337cf797094a514c723decbc2e8831
Roman Susi <roman.susi@kolumbus.fi>
2018-01-21 10:03:30 +0200

    Also added hasX example to the library

--- oMiser/mockups/python/library.py
+++ oMiser/mockups/python/library.py
@@ -1,6 +1,7 @@
-from miser import c, E, ARG, C, e, ob
+from miser import c, E, ARG, C, e, ob, NIL, EV, D, B, SELF, A, L
 
-cI = c(c(e(c(E, ARG)), ARG), c(e(c(E, ARG)), ARG))
+# cI = c(c(e(c(E, ARG)), ARG), c(e(c(E, ARG)), ARG))
+cI = NIL
 cK = c(E, ARG)  # K-combinator
 cSpart = c(C, c(cK, e(ARG)))
 cS = c(C, c(e(C), c(C, c(c(E, cSpart), e(cSpart)))))  # S-combinator
@@ -25,8 +26,8 @@ cB = c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E,
 cC = c(c(e(c(c(e(e(
     c(c(e(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG)))))))))), ARG),
       c(e(c(E, ARG)), ARG)))), ARG),
-             c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG))), ARG),
-       c(e(e(c(E, ARG))), ARG))
+    c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG))), ARG),
+    c(e(e(c(E, ARG))), ARG))
 
 # W = S S (S K)
 cW = c(c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, ARG), e(ARG))))))))), ARG),
@@ -34,5 +35,11 @@ cW = c(c(e(c(C, c(e(C), c(C, c(c(E, c(C, c(c(E, ARG), e(ARG)))), e(c(C, c(c(E, A
 
 # TODO: iota
 # cIOTA = ...
+
+#
+hasX = EV ** (D ** ARG ** B ** ARG) \
+       ** e(B ** (EV ** (D ** e(L("X")) ** A ** ARG)
+                  ** e(A ** (SELF ** B ** ARG))
+                  ))
 
 namespace = {k: v for k, v in vars().items() if isinstance(v, ob)}
